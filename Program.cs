bd9779185402c197eff07e53ffafb7d67ddbabe1
JamesonDuce <144130101+JamesonDuce@users.noreply.github.com>
2024-02-20 15:40:34 -0600

    Azure_Storage_Connected
    Azure storage_push

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.Extensions.Azure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddAzureClients(clientBuilder =>
+{
+    clientBuilder.AddBlobServiceClient(builder.Configuration["azureStorage:blob"]!, preferMsi: true);
+    clientBuilder.AddQueueServiceClient(builder.Configuration["azureStorage:queue"]!, preferMsi: true);
+});
 
 var app = builder.Build();
 
