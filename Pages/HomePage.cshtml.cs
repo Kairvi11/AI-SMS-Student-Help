4ba82ab72700a050da3fa4593c408a2cf6f8c0e3
JamesonDuce <144130101+JamesonDuce@users.noreply.github.com>
2024-02-20 13:49:43 -0600

    Home_Page
    Front end of the page which shows classes.

---
+++ Pages/HomePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AI_SMS_Student_Help.Pages
+{
+    public class HomePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
